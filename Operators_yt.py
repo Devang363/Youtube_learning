d47154117bc843de8bd4415cf8624fed80d90eb9
Devang363 <devangjadoun62@gamil.com>
2025-05-28 00:33:26 +0530

    Initial command

---
+++ Operators_yt.py
@@ -0,0 +1,30 @@
+a=int(input("Enter a number: "))
+
+b=int(input("Enter Another Number: "))
+
+avg=(a+b)/2
+
+print("Average of", a, "and", b, "is", avg)
+
+square=a**2
+
+print("Square of", a, "is", square)
+d = a >b 
+
+print("Is a greater than b?", d)
+
+t=type(a)
+
+print("Type of a is", t)
+
+c=type(b)
+
+print("Type of b is", c)
+
+sum=a+b
+
+remainder=a%b
+
+print("Sum of ",a,"and",b,"is",sum)
+
+print("Remainder of ",a,"and",b,"is",remainder)
