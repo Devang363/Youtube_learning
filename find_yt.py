7d7f7d1451c8835f3acd126ff552340f941144d3
Devang363 <devangjadoun62@gamil.com>
2025-05-31 23:43:22 +0530

    Youtube Fifth

---
+++ find_yt.py
@@ -0,0 +1,7 @@
+post = input("Enter the post : ")
+
+if ("Devang".lower() in post.lower()):
+    print("This post is talking about Devang")
+
+else:
+    print("This post is not talking about Devang")
